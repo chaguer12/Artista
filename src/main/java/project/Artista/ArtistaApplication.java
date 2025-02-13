c01e91851ab50028bfec5a1869a383bc61ad0807
zoolotoui <chag.med.amine@gmail.com>
2025-02-13 16:48:09 +0100

    creating project

---
+++ src/main/java/project/Artista/ArtistaApplication.java
@@ -0,0 +1,13 @@
+package project.Artista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/Artista/ArtistaApplicationTests.java
@@ -0,0 +1,13 @@
+package project.Artista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
