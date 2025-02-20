af24a0bb9defdd7db03426d789e0bb21c059e844
zoolotoui <chag.med.amine@gmail.com>
2025-02-20 17:29:49 +0100

    stat: finished blog post crud

--- src/main/java/project/Artista/controller/BlogPostController.java
+++ src/main/java/project/Artista/controller/BlogPostController.java
@@ -1,14 +1,13 @@
 package project.Artista.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.Artista.dto.records.blogPost.BlogReqDTO;
 import project.Artista.dto.records.blogPost.BlogResDTO;
+import project.Artista.dto.records.blogPost.BlogUpdateDTO;
 import project.Artista.service.BlogPostServiceInterface;
 
 import java.util.List;
@@ -21,7 +20,7 @@ public class BlogPostController {
     private final BlogPostServiceInterface blogPostService;
 
     @PostMapping("/add")
-    public ResponseEntity<BlogResDTO> save(BlogReqDTO blogDTO){
+    public ResponseEntity<BlogResDTO> save(@RequestBody @Valid BlogReqDTO blogDTO){
         BlogResDTO response = blogPostService.saveBlogPost(blogDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
@@ -29,5 +28,24 @@ public class BlogPostController {
     public ResponseEntity<List<BlogResDTO>> getAll(){
         List<BlogResDTO> response = blogPostService.getAllBlogPosts();
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    @GetMapping("/fetch/{id}")
+    public ResponseEntity<BlogResDTO> getBlogPost(@PathVariable int id){
+        BlogResDTO response = blogPostService.getBlogPostById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<BlogResDTO> update(@PathVariable int id, @Valid @RequestBody BlogUpdateDTO blogDTO){
+        BlogResDTO response = blogPostService.updateBlogPost(id,blogDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id){
+        blogPostService.deleteBlogPost(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+
     }
 }
