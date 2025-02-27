902ab1bbd1068d9d3104e3e91524bbca41d608be
zoolotoui <chag.med.amine@gmail.com>
2025-02-27 14:15:12 +0000

    stat: setting up cloudinary

--- src/main/java/project/Artista/ArtistaApplication.java
+++ src/main/java/project/Artista/ArtistaApplication.java
@@ -1,13 +1,15 @@
 package project.Artista;
 
+import com.cloudinary.Cloudinary;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ArtistaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtistaApplication.class, args);
+		
 	}
 
 }
